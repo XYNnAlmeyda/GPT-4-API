8b5c0880a4f6bbab70bc558dd36da9b81233484f
/XYNn <almeydakian7@gmail.com>
2024-10-14 14:23:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/chat', async (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3100;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
